b93e0b9662cad0f8900f59fede3f0df56236d143
Yuchi Deng <dengyuchi@gmail.com>
2017-04-09 01:29:33 -0700

    04/09 Add segmentation script

---
+++ segmentation/seg.py
@@ -0,0 +1,44 @@
+import os
+
+import jieba as jb
+import sys
+
+blank_chars = '　'
+blank_chars_replacement = ' '
+
+
+def trim_files(raw_file_dir):
+    for filename in os.listdir(raw_file_dir)[:3]:
+        if filename.endswith('.txt'):
+            with open(raw_file_dir + '/' + filename, 'r') as txt_file:
+                with open(raw_file_dir + '/trimmed/' + filename, 'w+') as output_file:
+                    print('Trimming file ' + filename)
+                    line_list = txt_file.readlines()
+                    line_list = [line.strip() for line in line_list]
+                    whole_text = str(''.join(line_list))
+                    whole_text = whole_text.translate(str.maketrans(blank_chars, blank_chars_replacement))
+                    whole_text = whole_text.replace(' ', '')
+                    print(whole_text)
+                    output_file.write(whole_text + "\n")
+                    output_file.close()
+                    txt_file.close()
+
+
+def seg_trimmed_files(raw_file_dir):
+    for filename in os.listdir(raw_file_dir)[:3]:
+        if filename.endswith('.txt'):
+            with open(raw_file_dir + '/' + '/trimmed/' + filename, 'r') as txt_file:
+                with open(raw_file_dir + '/segmented/' + filename, 'w+') as output_file:
+                    print('Segmenting file ' + filename)
+                    whole_text = txt_file.readline()
+                    seg_list = jb.cut(whole_text, cut_all=False)
+                    output_file.write("/ ".join(seg_list) + "\n")
+                    output_file.close()
+                    txt_file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        raw_file_dir = sys.argv[1]
+        trim_files(raw_file_dir)
+        seg_trimmed_files(raw_file_dir)
