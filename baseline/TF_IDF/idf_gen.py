0c318aeede1dd30de4d6aa80d708891885297882
AllanFly120 <zyp19930608@gmail.com>
2017-04-09 23:18:40 -0700

    tfidf

---
+++ baseline/TF_IDF/idf_gen.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import codecs
+import math
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+folder_path = sys.argv[1]
+output_path = './idf.txt'
+file_list = os.listdir(folder_path)
+total_num = len(file_list)
+idf = {}
+for file_name in file_list:
+	file_path = folder_path + '/' + file_name
+	str_in = open(file_path).read()
+	segmented = str_in.split('/ ')
+	word_set = set()
+	for word in segmented:
+		word_set.add(word)
+	for word in word_set:
+		if word not in idf:
+			idf[word] = 0
+		idf[word] += 1
+# print idf
+fout = open(output_path, 'w')
+except_count = 0
+for k, v in idf.iteritems():
+	try:
+		if len(k) > 100:
+			continue
+		# fout.write('{0} {1}\n'.format(k, str(math.log(total_num / v))))
+		fout.write('{0} {1}\n'.format(k, v))
+	except:
+		except_count += 1
+		continue
+fout.close()
+print except_count
---
+++ baseline/TF_IDF/it_idf.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import codecs
+import operator
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+input_path = sys.argv[1]
+idf_path = './idf.txt'
+
+class KeywordExtractor(object):
+	def __init__(self):
+		idf_file = open(idf_path, 'r')
+		self.idf = {}
+		print 'init idf data'
+		for index, line in enumerate(idf_file.readlines()):
+			try:
+				tmp_list = line.split(u' ')
+				self.idf[tmp_list[0]] = float(tmp_list[1])
+			except:
+				continue
+
+	def get_keywords(self, input_path, topK=20):
+		fin = open(input_path).read()
+		segmented = fin.split('/ ')
+		word_count = {}
+		for word in segmented:
+			if word.replace('.','',1).isdigit() or len(word) < 2:
+				continue
+			if word not in word_count:
+				word_count[word] = 0
+			word_count[word] += 1
+		for word in word_count:
+			count_idf = 0.
+			if word in self.idf:
+				count_idf = self.idf[word]
+			word_count[word] += count_idf
+		sorted_keywords = sorted(word_count.items(), key=operator.itemgetter(1), reverse=True)
+		rtn = []
+		for word, _ in sorted_keywords[:topK]:
+			rtn.append(word)
+		return rtn
+
+ke = KeywordExtractor()
+rtn = ke.get_keywords(input_path, 20)
+for word in rtn:
+	print word
+
